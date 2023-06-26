89c534099f8398427bc70d1d24b220466a762a48
Pawan2030 <136910101+Pawan2030@users.noreply.github.com>
2023-06-27 01:00:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ document.getElementById('cta-button').addEventListener('click', function() {
     var images = [
       'background1.jpg',
       'background2.jpg',
-      'background3.jpg'
+      'background3.jpg',
+        'background4.jpg',
+        'background5.jpg',
+        'background6.jpg',
+        'background7.jpg',
+      'background8.jpg'
     ];
     var randomIndex = Math.floor(Math.random() * images.length);
     var selectedImage = images[randomIndex];
